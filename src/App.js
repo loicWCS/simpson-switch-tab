519408a981c1d703924cc0a0dc8091b173fa2092
loicWCS <loic.lebon.wcs@gmail.com>
2022-07-06 14:30:22 +0200

    exo complet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [tab, setTab] = useState(true);
+
+  const handleReset = () => {
+    setQuotes([])
+  }
+    useEffect(() => {
+      return () => {
+        console.log('Show A se démonte')
+        handleReset()
+      }
+    }, [tab])
+  
+  /* appel API sur les boutons left et right*/
+  function getQuotes() {
+    // Send the request
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes?count=2")
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        console.log(data);
+        setQuotes(data);
+      });
+  }
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  function DisplayQuotes({ quotes }) {
+    return (
+      <div>
+        <button type="button" onClick={getQuotes} handleReset={handleReset}>
+          Get quotes
+        </button>
+        {quotes &&
+          quotes.map((el, id) => (
+            <div key={id}>
+              <img src={el.image} alt="" />
+              <ul>
+                <li>{el.quote}</li>
+                <li>{el.character}</li>
+                <li>{el.characterDirection}</li>
+              </ul>
+            </div>
+          ))}
+      </div>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setTab(!tab)}> Switch Tab</button>
+      <div className="grid">
+        <div className="left">
+          {tab ? <DisplayQuotes quotes={quotes} /> : null}
+        </div>
+
+        <div className="right">
+          {tab ? null : <DisplayQuotes quotes={quotes} />}
+        </div>
+      </div>
     </div>
   );
 }
